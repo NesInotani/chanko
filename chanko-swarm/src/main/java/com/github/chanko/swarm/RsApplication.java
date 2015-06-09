f9a9d25635b2afc91c876e3d94ff7f71023950c5
nobrooklyn <nobrooklyn@gmail.com>
2015-06-09 21:40:02 +0900

    add chanko-swarm

---
+++ chanko-swarm/src/main/java/com/github/chanko/swarm/RsApplication.java
@@ -0,0 +1,9 @@
+package com.github.chanko.swarm;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rs")
+public class RsApplication extends Application {
+
+}
